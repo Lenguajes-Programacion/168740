aa655fe500d785c7ea9dc94613f6fd0dde36a899
Erick Avila Alegria <60109218+EricScabbia@users.noreply.github.com>
2020-05-19 13:49:57 -0600

    menu para elegir programa

---
+++ clase_1/elegir programa/nombre.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElegirPrograma
+{
+    class Name
+    {
+        public string nombre = "Erick";
+        string n = "";
+        int contra = 6969;
+        int pass;
+        public void Erick()
+        {        
+            Console.WriteLine("Escribe el nombre");
+            n = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Escribe la contrasena");
+            pass = int.Parse(Console.ReadLine());
+
+            if (n == nombre && pass == contra) //true o false.
+            {
+                Console.WriteLine();
+                Console.WriteLine("Hello" + n);
+            }
+            else
+            {
+                Console.WriteLine("usuario autorizado");
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
